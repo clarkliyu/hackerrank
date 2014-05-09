7074cd861a2c5ee9b06e0aa04a3045966e44fb3d
Yu Li <liyu.buffalo@gmail.com>
2014-05-09 09:46:24 -0400

    done

---
+++ quickSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class quickSort{
+    static void quickSort(int[] ar) {
+
+    }   
+ 
+ 	static void printArray(int[] ar) {
+        for(int n: ar){
+           System.out.print(n+" ");
+        }
+           System.out.println("");
+    }
+       
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int i=0;i<n;i++){
+            ar[i]=in.nextInt(); 
+        }
+        quickSort(ar);
+    }   
+}
