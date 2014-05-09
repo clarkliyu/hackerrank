950a99afa380659393907c33654c0bc0b1da64fe
Yu Li <liyu.buffalo@gmail.com>
2014-05-09 14:20:42 -0400

    done

--- quickSort.java
+++ quickSort.java
@@ -1,10 +1,39 @@
 import java.util.*;
 
 public class quickSort{
-    static void quickSort(int[] ar) {
+    static void quickSort(int[] ar, int start, int end) {
+    	if(end-start<=1)
+    		return;
+    	int mid = partition(ar, start, end);
+    	quickSort(ar, start, mid);
+    	quickSort(ar, mid+1, end);
 
     }   
- 
+ 	
+    static int partition(int[] ar, int start, int end) {
+		List<Integer> greater= new ArrayList<Integer>();
+		List<Integer> less = new ArrayList<Integer>();
+		int p = ar[start];
+		for(int i=start+1; i<end; i++){
+			if(ar[i]>p){
+				greater.add(ar[i]);
+			}
+			else{
+				less.add(ar[i]);
+			}
+		}
+		for(int i=0; i<less.size();i++){
+			ar[start+i] = less.get(i);
+		}
+		ar[start+less.size()] = p;
+		for(int i=0; i<greater.size(); i++){
+			ar[start+i+less.size()+1] = greater.get(i);
+		}
+		int mid = start+less.size();
+		return mid;
+
+
+	}	
  	static void printArray(int[] ar) {
         for(int n: ar){
            System.out.print(n+" ");
@@ -19,6 +48,7 @@ public class quickSort{
         for(int i=0;i<n;i++){
             ar[i]=in.nextInt(); 
         }
-        quickSort(ar);
+        quickSort(ar, 0, n);
+        printArray(ar);
     }   
 }
