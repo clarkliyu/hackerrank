1285b206dd74b4d984d6ad51ed432e885add62b0
YU LI <liyu.buffalo@gmail.com>
2014-05-09 16:37:26 -0400

    Update FIzzBuzz.java

--- FIzzBuzz.java
+++ FIzzBuzz.java
@@ -8,7 +8,7 @@ public class FIzzBuzz{
 				System.out.println("FizzBuzz");
 			}
 			else if(i%3==0){
-				System.out.println("Fizz");
+				System.out.println("Fizz");   
 			}
 			else if(i%5==0){
 				System.out.println("Buzz");
