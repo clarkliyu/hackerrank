71fb0e14181b378e1fda49f3a6cf538277aead42
Yu Li <liyu.buffalo@gmail.com>
2014-05-12 22:57:07 -0400

    done

---
+++ lonelyInteger.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class lonelyinteger {
+    static int lonelyinteger(int[] a) {
+        int res=0;
+        int[] temp = new int[101];
+        for(int i=0; i<a.length; i++){
+            temp[a[i]]++;
+        }
+        
+        for(int i=0; i<=100; i++){
+            if(temp[i]==1){
+                res=i;
+                break;
+            }
+        }
+        return res;
+
+    }
+public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int res;
+        
+        int _a_size = Integer.parseInt(in.nextLine());
+        int[] _a = new int[_a_size];
+        int _a_item;
+        String next = in.nextLine();
+        String[] next_split = next.split(" ");
+        
+        for(int _a_i = 0; _a_i < _a_size; _a_i++) {
+            _a_item = Integer.parseInt(next_split[_a_i]);
+            _a[_a_i] = _a_item;
+        }
+        
+        res = lonelyinteger(_a);
+        System.out.println(res);
+        
+    }
+}
