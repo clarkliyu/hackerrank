12684cb32f0b3386381a6908d9381d35aab93d27
Yu Li <liyu.buffalo@gmail.com>
2014-06-28 00:18:14 -0400

    .

---
+++ coin.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class coin{
+	public static void main(String[] args){
+		Scanner myScan = new Scanner(System.in);
+		String s = myScan.nextLine();
+		String p = s.replaceAll("\\W","");
+		System.out.println(p);
+	}
+
+}
